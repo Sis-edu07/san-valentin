27bc1bc1b422f953b357ff788644a63a1e8fcf3d
Sis-edu07 <sis.edu07@gmail.com>
2025-02-14 16:19:10 -0600

    deploy v1 cart

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import audio from '../sound/ChristianBasso&HaienQiu-Flowers.mp3';
+import './css/inicio.css';
+import img1 from './../img/inicio1.jpg'
+import img2 from './../img/inicio2.jpg'
+import img3 from './../img/inicio3.jpg'
+import img4 from './../img/inicio4.jpg'
+
+
+export const HomePage = () => {
+  return (
+    <>
+      <div className='contenedor'>
+        <div className='encabezado-container'> 
+          <div id="brillo">¡Feliz Día de San Valentín! 💞</div>
+        </div>
+        <br/>
+        <br/>
+        <div className='galeria'>
+            <img src={img1} alt="" />
+            <img src={img2} alt="" />
+            <img src={img3} alt="" />
+            <img src={img4} alt="" />
+        </div>
+      </div>
+    </>
+  )
+}
